3e4f50f4749de83b355aa5f6c73d54a19a3841d1
Echchi <112493260+Echchi@users.noreply.github.com>
2024-01-24 23:24:28 +0900

    :lipstick: Font : 폰트 설정
    pretendard 폰트 설정

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="bg-indigo-700 h-1/2 w-2/3">
+        <div className="text-center text-7xl font-bold">memberee</div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
