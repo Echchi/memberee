34e65bf6dbeaa6011804ed35e2728fc6babaf4f2
Echchi <112493260+Echchi@users.noreply.github.com>
2024-01-26 22:16:10 +0900

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="bg-indigo-700 h-1/2 w-2/3">
-        <div className="text-center text-7xl font-bold">멤버리</div>
+        <div className="text-center text-7xl font-bold">memberee</div>
       </div>
     </div>
   );
