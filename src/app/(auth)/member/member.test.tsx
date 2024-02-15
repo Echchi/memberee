1e373dc2c580771973fb6859f36588794e1df5ca
Echchi <112493260+Echchi@users.noreply.github.com>
2024-02-15 23:35:27 +0900

    :construction: Progress:회원관리 페이지 테스트 코드 작성중
    회원관리 페이지 테스트 코드 작성중

---
+++ src/app/(auth)/member/member.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Main from "./page";
+import { useRouter } from "next/navigation";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
+describe("회원관리 메인 페이지", () => {
+  const mockPush = jest.fn();
+
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue({
+      push: mockPush,
+    });
+  });
+
+  test("회원관리 메인 페이지 렌더링", () => {
+    render(<Main />);
+    expect(
+      screen.getByPlaceholderText(/회원 이름, 연락처, 담당/i),
+    ).toBeInTheDocument();
+    const buttons = screen.getAllByRole("button", { name: /출력/i });
+    expect(buttons).toHaveLength(2);
+  });
+});
