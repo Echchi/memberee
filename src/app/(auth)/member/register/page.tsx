7396dd5ebb692b3efa801649fb1c7ebe191dd842
Echchi <112493260+Echchi@users.noreply.github.com>
2024-02-25 18:43:09 +0900

    :construction: Progress : 신규 회원 등록 구현중
    신규 회원 등록 구현중

--- src/app/(auth)/member/register/page.tsx
+++ src/app/(auth)/member/register/page.tsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import Input from "@/component/input";
 import { cls, formatCurrency } from "@/libs/client/utils";
 import Button from "@/component/button";
+import { format } from "date-fns";
 
 const Page = () => {
-  const [selectedDay, setSelectedDay] = useState<string[]>(["0"]);
+  const today = format(new Date(), "yyyy. MM. dd.");
+  const [selectedDay, setSelectedDay] = useState<string[]>(["월"]);
   const handleSelectDay = (day: string) => {
     if (selectedDay.includes(day)) {
       setSelectedDay((prev) => prev.filter((selected) => selected !== day));
@@ -46,107 +48,145 @@ const Page = () => {
         placeholder={"직업"}
         className="h-14 lg:text-lg border-b-1"
       />
-      <div className="col-span-2 h-24 border border-y-0 border-neutral-300 flex flex-col justify-center lg:pl-10 pl-4">
+      <div className="col-span-2 border border-y-0 border-neutral-300 flex flex-col justify-center lg:pl-10 pl-4 x *:text-base *:font-semibold *:text-stone-600">
         <div className="flex">
-          <span className="flex items-center lg:text-lg text-base font-bold text-stone-600">
-            요일 선택
-          </span>
+          <span className="flex items-center lg:text-lg">요일 선택</span>
           <div className="grow flex justify-center items-center">
             <div className="px-10 py-3 w-fit space-x-10 text-lg *:py-2 *:px-4 *:trnsition-all">
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("0")
+                  selectedDay.includes("월")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("0")}
+                onClick={() => handleSelectDay("월")}
               >
                 월
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("1")
+                  selectedDay.includes("화")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("1")}
+                onClick={() => handleSelectDay("화")}
               >
                 화
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("2")
+                  selectedDay.includes("수")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("2")}
+                onClick={() => handleSelectDay("수")}
               >
                 수
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("3")
+                  selectedDay.includes("목")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("3")}
+                onClick={() => handleSelectDay("목")}
               >
                 목
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("4")
+                  selectedDay.includes("금")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("4")}
+                onClick={() => handleSelectDay("금")}
               >
                 금
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("5")
+                  selectedDay.includes("토")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("5")}
+                onClick={() => handleSelectDay("토")}
               >
                 토
               </button>
               <button
                 className={cls(
                   "rounded-full text-stone-600",
-                  selectedDay.includes("6")
+                  selectedDay.includes("일")
                     ? "ring-2 ring-inset ring-emerald-600 font-bold"
                     : "bg-transparent font-medium",
                 )}
-                onClick={() => handleSelectDay("6")}
+                onClick={() => handleSelectDay("일")}
               >
                 일
               </button>
             </div>
           </div>
         </div>
+        <div className="flex">
+          <span className="flex items-center lg:text-lg">시간 선택</span>
+          <div className="grow flex items-center">
+            <div className="min-h-44 flex flex-col justify-center px-10 py-3 w-fit *:space-x-10 text-lg *:py-2 *:px-4 *:trnsition-all">
+              {selectedDay.map((day, index) => (
+                <div className="pl-0 w-full" key={index}>
+                  <span>{day}</span>
+                  <select className="outline-none bg-transparent">
+                    <option>오전</option>
+                    <option>오후</option>
+                  </select>
+                  <input
+                    className="outline-none bg-transparent"
+                    type="text"
+                    placeholder={"09:00"}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <Input
+          type={"text"}
+          label={"납부금액"}
+          placeholder={formatCurrency("250000")}
+          className="h-14 lg:text-lg border-b-0"
+        />
       </div>
       <Input
         type={"text"}
-        label={"납부금액"}
-        placeholder={formatCurrency("250000")}
+        label={"담당"}
+        placeholder={formatCurrency("이코치")}
         className="h-14 lg:text-lg border-b-1"
       />
       <Input
         type={"text"}
-        label={"메모"}
-        placeholder={"국민은행 000-000-00-000000"}
-        className="h-14 lg:text-lg rounded-br-lg border-l-0"
+        label={"시작 일자"}
+        placeholder={today}
+        className="h-14 lg:text-lg border-b-1 border-l-0"
       />
-      <Button text={"등록"} className="mt-4" large={true} />
+      <div className="col-span-2">
+        <Input
+          type={"text"}
+          label={"메모"}
+          placeholder={"국민은행 000-000-00-000000"}
+          isLong={true}
+          className="h-14 lg:text-lg rounded-b border-t-0"
+        />
+      </div>
+      <div className="col-span-2 flex justify-between space-x-10">
+        <Button text={"등록"} className="mt-4" large={true} />
+      </div>
     </div>
   );
 };
--- src/app/(auth)/member/register/register.test.tsx
+++ src/app/(auth)/member/register/register.test.tsx
@@ -12,9 +12,8 @@ describe("회원등록 모달", () => {
     expect(screen.getByText("연락처")).toBeInTheDocument();
     expect(screen.getByText("생년월일")).toBeInTheDocument();
     expect(screen.getByText("직업")).toBeInTheDocument();
-    expect(screen.getByText("요일")).toBeInTheDocument();
-    expect(screen.getByText("시간")).toBeInTheDocument();
-    expect(screen.getByText("금액")).toBeInTheDocument();
+    expect(screen.getByText("시간 선택")).toBeInTheDocument();
+    expect(screen.getByText("납부금액")).toBeInTheDocument();
     expect(screen.getByText("시작 일자")).toBeInTheDocument();
     expect(screen.getByText("담당")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /등록/i }));
