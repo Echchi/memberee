d5f08a935836603c5c7107b7b29b1f5c9b47804e
Echchi <112493260+Echchi@users.noreply.github.com>
2024-02-26 19:14:35 +0900

    :construction: Progress : 타임피커 컴포넌트 작업중
    타임피커 컴포넌트 작업중

---
+++ src/component/test/timepicker.test.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Button from "../timepicker";
+
+describe("타임피커 컴포넌트", () => {});
---
+++ src/component/timepicker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timepicker = () => {
+  return <div></div>;
+};
+
+export default Timepicker;
