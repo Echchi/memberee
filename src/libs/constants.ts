1743e219c5179ffcfce0a6a10b5c7ea848173781
Echchi <112493260+Echchi@users.noreply.github.com>
2024-03-16 21:13:00 +0900

    commit

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,6 +1,5 @@
 export const ID_REGEX = new RegExp(/^[a-z]+[a-z0-9]{5,19}$/g);
-export const ID_REGEX_ERROR =
-  "영문자로 시작하는 영문자 또는 숫자 6 ~ 10 글자여야해요";
+export const ID_REGEX_ERROR = "영문자 또는 숫자 6 ~ 10 글자여야해요";
 export const PASSWORD_REGEX = new RegExp(
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/,
 );
