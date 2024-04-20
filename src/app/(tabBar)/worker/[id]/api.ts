75d27df49fa3684bcbf356e10d4e4be9c35c588e
Echchi <112493260+Echchi@users.noreply.github.com>
2024-04-20 23:48:57 +0900

    commit

--- src/app/(tabBar)/worker/[id]/api.ts
+++ src/app/(tabBar)/worker/[id]/api.ts
@@ -1,5 +1,8 @@
 "use server";
+import session from "@/libs/client/session";
 import db from "@/libs/server/db";
+import getSession from "@/libs/client/session";
+import { formatISODate } from "@/libs/client/utils";
 
 export async function getWorker(id: number) {
   const worker = await db.worker.findUnique({
@@ -21,4 +24,18 @@ export async function getMembers(id: number) {
     },
   });
   return members;
+}
+
+export async function terminateWorker(id: number) {
+  const session = await getSession();
+  const companyId = session.company;
+  const worker = await db.worker.update({
+    where: {
+      id,
+      companyId,
+    },
+    data: {
+      status: 0,
+    },
+  });
 }
