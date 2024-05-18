046b8c5fbbe10f5116104d09e819637f01e6a345
Echchi <112493260+Echchi@users.noreply.github.com>
2024-05-18 21:13:10 +0900

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,27 +21,27 @@ const Login = () => {
         </div>
 
         <div className="w-full h-32 mt-16">
-          {/*<Input*/}
-          {/*  required={true}*/}
-          {/*  type={"text"}*/}
-          {/*  icon={*/}
-          {/*    <svg*/}
-          {/*      xmlns="http://www.w3.org/2000/svg"*/}
-          {/*      viewBox="0 0 24 24"*/}
-          {/*      fill="currentColor"*/}
-          {/*      className="w-6 h-6 text-gray-300"*/}
-          {/*    >*/}
-          {/*      <path*/}
-          {/*        fillRule="evenodd"*/}
-          {/*        d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"*/}
-          {/*        clipRule="evenodd"*/}
-          {/*      />*/}
-          {/*    </svg>*/}
-          {/*  }*/}
-          {/*  placeholder={"아이디"}*/}
-          {/*  className="h-1/2 rounded-t-xl"*/}
-          {/*  name="userid"*/}
-          {/*/>*/}
+          <Input
+            required={true}
+            type={"text"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 text-gray-300"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            }
+            placeholder={"아이디"}
+            className="h-1/2 rounded-t-xl"
+            name="userid"
+          />
           <Input
             required={true}
             type={"password"}
