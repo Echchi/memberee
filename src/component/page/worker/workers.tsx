c0d26bf7a4adec2d6b55d25cb633fd9adf736a7b
Echchi <112493260+Echchi@users.noreply.github.com>
2024-05-30 15:36:15 +0900

    commit

--- src/component/page/worker/workers.tsx
+++ src/component/page/worker/workers.tsx
@@ -47,7 +47,20 @@ const Workers = async ({ query }: { query?: string }) => {
             key={worker.id}
             className="bg-white w-full min-h-fit shadow rounded-lg flex flex-col items-center p-4 hover:shadow-lg"
           >
-            <div className="bg-stone-200 rounded-full h-32 w-32" />
+            <div className="bg-stone-200 rounded-full h-32 w-32 flex justify-center items-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 text-neutral-400 w-20 h-20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
             <div className="mt-4 flex flex-col items-center space-y-1">
               <p className="font-bold text-lg tracking-wider">{worker.name}</p>
               <p className="space-x-2">
