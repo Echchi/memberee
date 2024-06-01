16cb75ebad31f81affe54f7a3a4ff3621930ea12
Echchi <112493260+Echchi@users.noreply.github.com>
2024-06-01 15:32:02 +0900

    commit

--- src/component/page/main/worker.tsx
+++ src/component/page/main/worker.tsx
@@ -43,7 +43,20 @@ const Worker = ({ workers }: { workers: WorkerWithMember[] }) => {
                 key={`main_worker_${worker.id}`}
                 className="flex flex-col justify-evenly items-center rounded-lg min-x-fit px-5 py-2 hover:bg-stone-100 cursor-pointer transition-all"
               >
-                <div className="rounded-full bg-gray-300 mb-4 h-20 w-20" />
+                {/*<div className="rounded-full bg-gray-300 mb-4 h-32 aspect-square" />*/}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6 text-gray-400 w-10 h-10  mb-2"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
                 <p className="text-base lg:text-lg font-semibold">
                   {worker.name}
                 </p>
