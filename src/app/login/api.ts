799e365782c794e9c8c62beb2460dd41b0b8cd06
Echchi <112493260+Echchi@users.noreply.github.com>
2024-07-03 19:00:04 +0900

    commit

--- src/app/login/api.ts
+++ src/app/login/api.ts
@@ -3,6 +3,7 @@ import db from "@/libs/server/db";
 import { Resend } from "resend";
 import React from "react";
 import FindPasswordEmail from "../../../emails/find-password-email";
+import { ITmpEmail } from "@/app/login/join";
 
 export interface FindParam {
   phone: string;
@@ -90,3 +91,55 @@ export const sendPasswordEmail = async (param: SendPasswordEmail) => {
   });
   return res ? { success: true } : { success: false };
 };
+
+export async function checkEmail(email: string) {
+  const user = await db.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  return user;
+}
+
+export async function checkTmpEmail(email: string) {
+  const tmpEmail = await db.tmpEmail.findFirst({
+    where: {
+      email,
+    },
+    select: {
+      id,
+      email,
+      expiresAt,
+    },
+  });
+
+  return tmpEmail;
+}
+
+export async function updateTmpEmail(params: ITmpEmail) {
+  const { id, email, token, expiresAt } = params;
+  const tmpEmail = await db.tmpEmail.update({
+    where: {
+      id,
+    },
+    data: {
+      token,
+      expiresAt,
+    },
+  });
+
+  return tmpEmail;
+}
+export async function createTmpEmail(params: ITmpEmail) {
+  const { email, token, expiresAt } = params;
+  const tmpEmail = await db.tmpEmail.create({
+    data: {
+      email,
+      token,
+      expiresAt,
+    },
+  });
+
+  return tmpEmail;
+}
