9a4efce577d6e6484c9e51f91cece0140d62f795
Echchi <112493260+Echchi@users.noreply.github.com>
2024-07-08 23:11:25 +0900

    :zap: Improve:회원, 직원 엑셀 등록 로딩창 및 문구 수정

--- src/component/page/main/BulkUploadHandlers/worker/workerExcelModal.tsx
+++ src/component/page/main/BulkUploadHandlers/worker/workerExcelModal.tsx
@@ -36,6 +36,7 @@ const WorkerExcelModal = ({ onClose }: { onClose: () => void }) => {
           console.error("Error reading file: ", error);
         });
     }
+    event.target.value = "";
   };
 
   useEffect(() => {
@@ -71,9 +72,9 @@ const WorkerExcelModal = ({ onClose }: { onClose: () => void }) => {
       ) : (
         <>
           <div className="h-20 flex justify-between space-x-3 py-3 w-full">
-            <div className="flex w-1/2">
+            <div className="flex w-full xl:w-1/2">
               <label className="w-32 stone_btn font-semibold text-center flex justify-center items-center px-3 py-2 cursor-pointer">
-                파일 선택
+                파일 등록
                 <input
                   type="file"
                   className="hidden"
@@ -87,64 +88,87 @@ const WorkerExcelModal = ({ onClose }: { onClose: () => void }) => {
                 className="rounded-r-lg px-3"
               />
             </div>
-            <div className="w-1/6">
-              <RegisterWorkers />
-            </div>
+            {listData.length > 0 && selecetdFile && (
+              <div className="w-1/6">
+                <RegisterWorkers />
+              </div>
+            )}
           </div>
-          <div className="h-[500px] my-3 overflow-y-scroll">
-            <table className="w-full table-auto text-center border-stone-100">
-              <thead className="*:text-lg font-semibold bg-stone-100 h-16 sticky top-0">
-                <tr>
-                  <td>이름</td>
-                  <td>연락처</td>
-                  <td>생년월일</td>
-                  <td>시작일자</td>
-                  <td>근무일</td>
-                  <td>수수료</td>
-                  <td>은행</td>
-                  <td>계좌번호</td>
-                </tr>
-              </thead>
-              <tbody className="*:h-10">
-                {listData &&
-                  listData?.map((items: string[], idx) => (
-                    <tr
-                      key={`register_workers_${idx}`}
-                      className={cls(
-                        "border border-stone-100 border-x-0 border-t-0 border-b",
-                        errors && errors.includes(idx)
-                          ? "bg-orange-200 text-orange-500"
-                          : "",
-                      )}
-                    >
-                      {items.map((item: string, index) => (
-                        <td key={`register_worker_${idx}-${index}`}>{item}</td>
-                      ))}
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+          <div className="h-[495px] my-3 overflow-y-scroll">
+            {listData.length > 0 && selecetdFile ? (
+              <table className="w-full table-auto text-center border-stone-100">
+                <thead className="*:text-lg font-semibold bg-stone-100 h-16 sticky top-0">
+                  <tr>
+                    <td>이름</td>
+                    <td>연락처</td>
+                    <td>생년월일</td>
+                    <td>시작일자</td>
+                    <td>근무일</td>
+                    <td>수수료</td>
+                    <td>은행</td>
+                    <td>계좌번호</td>
+                  </tr>
+                </thead>
+                <tbody className="*:h-10">
+                  {listData &&
+                    listData?.map((items: string[], idx) => (
+                      <tr
+                        key={`register_workers_${idx}`}
+                        className={cls(
+                          "border border-stone-100 border-x-0 border-t-0 border-b",
+                          errors && errors.includes(idx)
+                            ? "bg-orange-200 text-orange-500"
+                            : "",
+                        )}
+                      >
+                        {items.map((item: string, index) => (
+                          <td key={`register_worker_${idx}-${index}`}>
+                            {item}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            ) : (
+              <div className="w-full h-full flex flex-col justify-center items-center text-sm xl:text-lg xl:space-y-2 space-y-1">
+                <p>
+                  양식을 다운로드하시고,
+                  <span className="text-red-600"> 빨간 글씨</span>에 맞게 직원
+                  정보를 입력해주세요
+                </p>
+                <p>
+                  그 다음, 왼쪽 위에 파일을 등록해주시면 등록을 도와드릴게요!
+                </p>
+                <div className="w-1/2 xl:w-1/4 pt-3">
+                  <RegisterWorkers />
+                </div>
+              </div>
+            )}
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between pb-3">
             <p
               className={cls(
                 "flex justify-center items-center font-semibold text-xl",
                 errors.length > 0 ? "text-orange-500" : "text-emerald-700",
               )}
             >
-              {errors.length > 0
-                ? `${errors.length}개의 데이터를 확인해주세요`
-                : `총 ${listData.length} 명`}
+              {listData.length > 0 &&
+                selecetdFile &&
+                (errors.length > 0
+                  ? `${errors.length}개의 데이터가 잘못되어있어요`
+                  : `총 ${listData.length} 명`)}
             </p>
-
-            <WorkersUploadBtn
-              listData={listData}
-              errors={errors}
-              isLoading={isLoading}
-              setIsLoading={setIsLoading}
-              onClose={onClose}
-              setProgress={setProgress}
-            />
+            {listData.length > 0 && selecetdFile && (
+              <WorkersUploadBtn
+                listData={listData}
+                errors={errors}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                onClose={onClose}
+                setProgress={setProgress}
+              />
+            )}
           </div>
         </>
       )}
