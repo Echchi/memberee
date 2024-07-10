b284eb35c92123424d3307ad7565e4b1e64476a7
Echchi <112493260+Echchi@users.noreply.github.com>
2024-07-11 00:08:48 +0900

    commit

--- src/component/page/landing/start.tsx
+++ src/component/page/landing/start.tsx
@@ -1,10 +1,13 @@
+"use client";
 import React from "react";
+import Button from "@/component/button/button";
+import { useRouter } from "next/navigation";
 
 const Introduction = () => {
+  const router = useRouter();
   return (
     <div className="w-full flex flex-col justify-center items-center text-6xl md:text-9xl space-y-3">
       <p>시작</p>
-      <p>어쩌구 저쩌구</p>
       <p>어쩌구 저쩌구</p>
       <p>어쩌구 저쩌구</p>
     </div>
